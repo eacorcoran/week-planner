1a065c5e38439e7798cbc3628d558ff6d38e66a7
Erin Corcoran <corcoran.erin.a@gmail.com>
2024-07-31 02:34:14 +0000

    Issue 1: Updating local storage with new event information.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* global data */
 /* event listener for if a user tries to add a new entry */
 var $dialog = document.querySelector('dialog');
 if (!$dialog)
@@ -9,8 +10,36 @@ $newEntryClick.addEventListener('click', function (event) {
     var $EventTarget = event.target;
     if (!$EventTarget)
         throw new Error('$eventTarget is null');
-    /* checking to make sure that delete-entry is selected */
+    /* checking to make sure that new event is selected */
     if ($EventTarget.matches('.new-event')) {
         $dialog.showModal();
     }
+});
+var $form = document.querySelector('form');
+if (!$form)
+    throw new Error('$form is null');
+/* listener for when user submits new event entry */
+$form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var $formElements = $form.elements;
+    var $dialog = document.querySelector('dialog');
+    if (!$dialog)
+        throw Error('$dialog does not exist');
+    /* event id should always increment so that no two events share an event id */
+    var eventID = data.nextEventId;
+    var eventDetail = {
+        eventID: eventID,
+        time: $formElements.time.value,
+        day: $formElements.day.value,
+        info: $formElements.info.value,
+    };
+    /* adding the new event to the exiting data object */
+    data.events.push(eventDetail);
+    /* entry id should always increment so that no two entries share an entry id */
+    data.nextEventId = eventID + 1;
+    /* form is reset */
+    $form.reset();
+    /* entries are written to local storage */
+    writeEntries();
+    $dialog.close();
 });
--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+/* global data */
+
+interface formElements extends HTMLFormControlsCollection {
+  time: HTMLInputElement;
+  day: HTMLInputElement;
+  info: HTMLInputElement;
+}
+
 /* event listener for if a user tries to add a new entry */
 const $dialog = document.querySelector('dialog');
 if (!$dialog) throw Error('$dialog does not exist');
@@ -9,8 +17,44 @@ $newEntryClick.addEventListener('click', function (event) {
   const $EventTarget = event.target as HTMLElement;
   if (!$EventTarget) throw new Error('$eventTarget is null');
 
-  /* checking to make sure that delete-entry is selected */
+  /* checking to make sure that new event is selected */
   if ($EventTarget.matches('.new-event')) {
     $dialog.showModal();
   }
+});
+
+const $form = document.querySelector('form') as HTMLFormElement;
+if (!$form) throw new Error('$form is null');
+
+/* listener for when user submits new event entry */
+$form.addEventListener('submit', (event: Event) => {
+  event.preventDefault();
+
+  const $formElements = $form.elements as formElements;
+
+  const $dialog = document.querySelector('dialog');
+  if (!$dialog) throw Error('$dialog does not exist');
+
+  /* event id should always increment so that no two events share an event id */
+  const eventID = data.nextEventId;
+  const eventDetail: eventFormat = {
+    eventID: eventID,
+    time: $formElements.time.value,
+    day: $formElements.day.value,
+    info: $formElements.info.value,
+  };
+
+  /* adding the new event to the exiting data object */
+  data.events.push(eventDetail);
+
+  /* entry id should always increment so that no two entries share an entry id */
+  data.nextEventId = eventID + 1;
+
+  /* form is reset */
+  $form.reset();
+
+  /* entries are written to local storage */
+  writeEntries();
+
+  $dialog.close();
 });
